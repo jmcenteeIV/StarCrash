423c5c6e6ae92916e10876e5b1ee8e9a8106dfa4
Jansen McEntee <jmcenteeiv@gmail.com>
2022-01-28 15:12:20 -0700

    remove random comments

--- lib/utility.py
+++ lib/utility.py
@@ -2,9 +2,6 @@ import os
 import sys
 
 def create_resource_path(resource_path):
-    # for root, dirs, files in os.walk(".\\assets", topdown=False):
-    #     for file in files:
-    #         print(f'{root}\\{file}')
     try:
     # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
