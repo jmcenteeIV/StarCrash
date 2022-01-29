40893cf335b18d4d0db4946aff2cdd23ca56e468
matt <noemail@gmail.com>
2022-01-29 14:28:27 -0700

    broke git and started over

--- lib/baddies.py
+++ lib/baddies.py
@@ -2,17 +2,17 @@ from pygame.sprite import Sprite
 from pygame.constants import K_LEFT, K_RIGHT, K_DOWN, K_UP
 
 import pygame
-import random
 
-# applies a 2-Dimensional Vector
+
 vec = pygame.math.Vector2
 
-class bad_guy(Sprite):
+class Baddies(pygame.sprite.Sprite):
     """
-    Currently repersents a single enemy, but can be used to spawn multiple enemies when used as an object
+    Currently repersents a single enemy
     """
 
-    def __init__(self, height, width, acceleration, friction):
+    # TODO (matthew.moroge) will probably need to the class for the screen as a param and use super()
+    def __init__(self, width, height, start_position, speed, aggression):
         super().__init__()
         """
         Initialize the alien and set its starting position
@@ -23,42 +23,30 @@ class bad_guy(Sprite):
         # self.image.convert_alpha()
         # self.rect = self.enemy.image.get_rect()
 
-        # basic handling for enemy movement 
-        self.friction = friction
-        self.acceleration = acceleration
-
-        # screen variables
-        self.width = width
-        self.height = height
-
         # creating basic rect for beginning, change to image later
         # gonna need to import the screen variable for the rect
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((128,255,40))
-        self.rect = pygame.draw.rect((self.screen, 0, 0, 255), (20, 20, 160,160))
 
+        self.width = width
+        self.height = height
+        self.aggression = aggression
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect( center = (50,50))
 
-        # enemy base health, can be changed
-        self.health = 10
+        # self.rect = pygame.draw.rect((screen, 0, 0, 255), (20, 20, 160,160))
+
+        self.speed = speed
+        self.counter = 0
+        self.pos = vec(start_position)
         
-        # pos: starting position for enemy 
-        # vel: elocity with which it should move across the window
-        # acc: enemy acceleration
-        self.pos = vec((width/2, height))
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
 
-    def blitme(self):
-        """Draw the enemy at its current location."""
-        self.surf.blit(self.image, self.rect)
-
+    def update(self):
+        self.move()
 
     def move(self):
         """
         moving the enemy
-        """   
-        free_real_estate = ['top', 'topleft', 'topright', 'midtop', 'midleft', 'midright', ]
-        randy_appear = random.choice(free_real_estate)
+        """
 
         self.pos.x += self.speed
 
@@ -67,22 +55,6 @@ class bad_guy(Sprite):
         if self.pos.x < (self.rect.width/2):
             self.pos.x, self.speed = (self.rect.width/2) , self.speed * -1
 
-        # starting position for enemy spawn before movement 
-        # starting at top left of screen
-        self.rect.topleft = self.pos
-
-    def update(self):
-        """
-        updating enemy movement to the screen 
-        """
-        self.move()
-
-        # handling co
-
-    def switch_mode(self):
-        """
-        switching to mech mode
-        """
-        pass 
+        self.rect.midbottom = self.pos
 
     
