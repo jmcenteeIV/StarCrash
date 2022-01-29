c7fc17a3add713dbf028e987af5c5f9e912bc264
matt <noemail@gmail.com>
2022-01-29 09:51:19 -0700

    better understanding of movement, updated to reflect better patterns

--- lib/baddies.py
+++ lib/baddies.py
@@ -1,10 +1,12 @@
 from re import X
 from pygame.sprite import Sprite
+from pygame.constants import K_LEFT, K_RIGHT, K_DOWN, K_UP
 import Game
 import loader
 import pygame
 
-
+# applies a 2-Dimensional Vector
+vec = pygame.math.Vector2
 
 class bad_guy(Sprite):
     """
@@ -12,7 +14,7 @@ class bad_guy(Sprite):
     """
 
     # TODO (matthew.moroge) will probably need to the class for the screen as a param and use super()
-    def __init__(self, x, y):
+    def __init__(self, height, width, acceleration, friction):
         """
         Initialize the alien and set its starting position
         """
@@ -22,46 +24,92 @@ class bad_guy(Sprite):
         # self.image.convert_alpha()
         # self.rect = self.enemy.image.get_rect()
 
+        # basic handling for enemy movement 
+        self.friction = friction
+        self.acceleration = acceleration
+
+        # screen variables
+        self.width = width
+        self.height = height
+
         # creating basic rect for beginning, change to image later
         # gonna need to import the screen variable for the rect
-        self.screen = pygame.Surface((50, 50))
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128,255,40))
         self.rect = pygame.draw.rect((self.screen, 0, 0, 255), (20, 20, 160,160))
 
-        # base enemy speed, trying out 10, can be changed 
-        self.enemy_speed = 10
 
         # enemy base health, can be changed
         self.health = 10
         
-        # x: horizantial movement 
-        # y: vertical movement 
-        # counter: keeping track of movement axis
-        self.rect.x = x 
-        self.rect.y = y 
-        self.counter = 0
+        # pos: starting position for enemy 
+        # vel: elocity with which it should move across the window
+        # acc: enemy acceleration
+        self.pos = vec((width/2, height))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+
+    def blitme(self):
+        """Draw the enemy at its current location."""
+        self.surf.blit(self.image, self.rect)
 
 
     def move(self):
         """
         moving the enemy
         """
-        # adjust to screen size for better movement strides 
-        distance = 80
+        self.acc = vec(0,0)
+    
+        pressed_keys = pygame.key.get_pressed()
 
-        if self.counter >= 0 and self.counter <= distance:
-            self.rect.x += self.enemy_speed
-        elif self.counter >= distance and self.counter <= distance * 2:
-            self.rect.x -= self.enemy_speed
-        else:
-            self.counter = 0
+        #  Horizontal movement for key presses 
+        # x: hortizontal axis movement         
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -self.acceleration
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = self.acceleration
 
-        self.counter += 1
+        self.acc.x += self.vel.x * self.friction
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > (self.width - (self.rect.width/2)):
+            self.pos.x, self.acc.x = (self.width - (self.rect.width/2)), 0
+        if self.pos.x < (self.rect.width/2):
+            self.pos.x, self.acc.x = (self.rect.width/2) , 0
+
+        #  Vertical movement for key presses
+        # y: vertical axis movement 
+        if pressed_keys[K_DOWN]:
+            self.acc.y = self.acceleration
+        if pressed_keys[K_UP]:
+            self.acc.y = -self.acceleration
+
+        # applying fricition to enemey movement 
+        self.acc.y += self.vel.y * self.friction
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.y > self.height:
+            self.pos.y, self.acc.y = self.height, 0
+        if self.pos.y < self.rect.height:
+            self.pos.y, self.acc.y = (0 + self.rect.height), 0
+
+        # starting position for enemy spawn before movement 
+        # starting at top left of screen
+        self.rect.topleft = self.pos
+
+    def update(self):
+        """
+        updating enemy movement to the screen 
+        """
+        self.move()
 
     def close_to_edge(self):
         """
         checking if enemy is at edge of screen
         """
-        screen_rect = self.screen.get_rect()
+        screen_rect = self.surf.get_rect()
 
         if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
