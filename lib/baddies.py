cd8cc477036681f0de85d9e9f00e44c590153274
matt <noemail@gmail.com>
2022-01-29 09:29:29 -0700

    updated enemy to adjust basic movement

--- lib/baddies.py
+++ lib/baddies.py
@@ -1,5 +1,6 @@
 from re import X
 from pygame.sprite import Sprite
+import Game
 import loader
 import pygame
 
@@ -7,7 +8,7 @@ import pygame
 
 class bad_guy(Sprite):
     """
-    Currently repersents a single enemy
+    Currently repersents a single enemy, but can be used to spawn multiple enemies when used as an object
     """
 
     # TODO (matthew.moroge) will probably need to the class for the screen as a param and use super()
@@ -23,11 +24,18 @@ class bad_guy(Sprite):
 
         # creating basic rect for beginning, change to image later
         # gonna need to import the screen variable for the rect
+        self.screen = pygame.Surface((50, 50))
+        self.rect = pygame.draw.rect((self.screen, 0, 0, 255), (20, 20, 160,160))
 
-        self.rect = pygame.draw.rect((screen, 0, 0, 255), (20, 20, 160,160))
-
+        # base enemy speed, trying out 10, can be changed 
         self.enemy_speed = 10
 
+        # enemy base health, can be changed
+        self.health = 10
+        
+        # x: horizantial movement 
+        # y: vertical movement 
+        # counter: keeping track of movement axis
         self.rect.x = x 
         self.rect.y = y 
         self.counter = 0
@@ -37,6 +45,7 @@ class bad_guy(Sprite):
         """
         moving the enemy
         """
+        # adjust to screen size for better movement strides 
         distance = 80
 
         if self.counter >= 0 and self.counter <= distance:
