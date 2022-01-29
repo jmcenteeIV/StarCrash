8325c7503a610add7262bdf85f1088933dfc2d49
matt <noemail@gmail.com>
2022-01-28 22:02:10 -0700

    carved out more basic methods for enemy class

--- lib/baddies.py
+++ lib/baddies.py
@@ -13,7 +13,7 @@ class bad_guy(Sprite):
         """
         Initialize the alien and set its starting position
         """
-
+        
         # load enemy image when we have it
         # self.image = pygame.image.load('path/to/image')
         # self.rect = self.enemy.image.get_rect()
@@ -21,10 +21,14 @@ class bad_guy(Sprite):
         # creating basic rect for beginning, change to image later
         # gonna need to import the screen variable for the rect
 
-        self.rect = pygame.draw.rect((0, 0, 255), (20, 20, 160,160))
+        self.rect = pygame.draw.rect((screen, 0, 0, 255), (20, 20, 160,160))
+
         # Starting each enemy near the top left of screen, can change later
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
+
+        # enemy speed
+        self.enemy_speed = 1.0
 
         # tracking enemy horizontal speed for fine tuning later on
         self.x = float(self.rect.x)
@@ -32,3 +36,26 @@ class bad_guy(Sprite):
         # tracking enemy vertical speed for fine tuning later on
         self.y = float(self.rect.y)
 
+
+    def move(self):
+        """
+        moving the enemy
+        """
+        self.x += self.enemy_speed
+        self.rect.x = self.x 
+
+    def close_to_edge(self):
+        """
+        checking if enemy is at edge of screen
+        """
+        screen_rect = self.screen.get_rect()
+
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """
+        moving enemy left or right
+        """
+        self.x += self.enemy_speed
+        self.rect.x = self.x 
