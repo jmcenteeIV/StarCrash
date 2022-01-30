578b921000fff9c76e1747aa2a76fcc1ea088a4d
matt <noemail@gmail.com>
2022-01-30 10:57:09 -0700

    added random sound selection for enemy fire

--- lib/bullet.py
+++ lib/bullet.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.constants import K_LEFT, K_RIGHT, K_DOWN, K_UP
 
 from lib import resources
@@ -7,7 +8,7 @@ vec = pygame.math.Vector2
 
 class Bullet(pygame.sprite.Sprite):
 
-    def __init__(self, velocity, pos, enemy_bullet, image):
+    def __init__(self, velocity, pos, enemy_bullet, image,):
         super().__init__()
         #Sprite Properties
         self.image = image
@@ -22,6 +23,10 @@ class Bullet(pygame.sprite.Sprite):
         self.vel = vec(0, velocity)
         self.enemy_bullet = enemy_bullet
 
+        # Sound Properties
+        self.enemy_shots = [pygame.mixer.Sound(f"/home/jammer/git/upsidedown-postman/assets/sounds/shots{x}.wav") for x in range(1,3)]
+        self.rand_shots = random.choice(self.enemy_shots)
+
     def update(self):
         self.move()
     
@@ -30,6 +35,7 @@ class Bullet(pygame.sprite.Sprite):
             self.pos -= self.vel
         else:
             self.pos += self.vel
+            self.rand_shots.play()
         if self.pos.y < 0 - self.rect.height or self.pos.y > self.game.height:
             self.kill()
         self.rect.midbottom = self.pos
