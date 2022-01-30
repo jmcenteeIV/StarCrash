0063ffbf3bad79121538ed75dfbd11cbe1cd9d3e
Rodney Rodriguez <rrodrig6@uccs.edu>
2022-01-30 05:32:11 -0700

    Add mode and liftime options for AnimatedEffect

--- lib/animatedeffect.py
+++ lib/animatedeffect.py
@@ -5,7 +5,7 @@ from lib import resources
 
 class AnimatedEffect(pygame.sprite.Sprite):
 
-    def __init__(self, pos, images, animation_rate, lifetime):
+    def __init__(self, pos, images, animation_rate: int=10, lifetime: int=0, playback_mode: int=0):
         super().__init__()
         self.res = resources.Resources.instance()
         self.images = images
@@ -16,21 +16,36 @@ class AnimatedEffect(pygame.sprite.Sprite):
         self.res.update_groups["game"].add(self)
         self.res.draw_groups["render"].add(self)
 
-        self.time_count = 0         # Keeps track of game ticks since spawn
+        self.time_count = 0                     # Keeps track of game ticks since spawn
         self.animation_rate = animation_rate    # Update the animation frame every x ticks
-        self.lifetime = lifetime         # Destroy this sprite when the time is up
+        self.lifetime = lifetime                # Destroy this sprite when the time is up (when animation ends when set to 0)
+        self.playback_mode = playback_mode      # 0: Looping, 1: PingPong
+        self.animation_direction = 1            # Used by playback_mode
+        self.animation_ended = False            # True when animation ends
 
     def update(self):
         self.time_count = self.time_count + 1
 
         if self.time_count % self.animation_rate == self.animation_rate -1:
-            self.current_frame = self.current_frame + 1
+            self.current_frame = self.current_frame + self.animation_direction
             if self.current_frame >= len(self.images):
+                if self.playback_mode == 0:
+                    self.current_frame = 0
+                    self.animation_ended = True
+                if self.playback_mode == 1:
+                    self.animation_direction = -1
+                    self.current_frame = len(self.images) - 1
+            if self.current_frame < 0:
+                self.animation_direction = 1
                 self.current_frame = 0
+                self.animation_ended = True
 
         self.image = self.images[self.current_frame]
 
-        if self.time_count >= self.lifetime:
+        if self.lifetime == 0:
+            if self.animation_ended:
+                self.destroy()
+        elif self.time_count >= self.lifetime:
             self.destroy()
 
     def destroy(self):
