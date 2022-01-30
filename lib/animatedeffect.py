48b3f1015bf5de557bb7adb3dfbf0ee0bcba941b
Rodney Rodriguez <rrodrig6@uccs.edu>
2022-01-30 05:08:26 -0700

    Add AnimatedEffect, derive Explosion from it

---
+++ lib/animatedeffect.py
@@ -0,0 +1,38 @@
+import random
+import pygame
+
+from lib import resources
+
+class AnimatedEffect(pygame.sprite.Sprite):
+
+    def __init__(self, pos, images, animation_rate, lifetime):
+        super().__init__()
+        self.res = resources.Resources.instance()
+        self.images = images
+        self.current_frame = 0
+        self.image = self.images[self.current_frame]
+        self.rect = pygame.Rect(pos[0], pos[1], 1, 1)
+
+        self.res.update_groups["game"].add(self)
+        self.res.draw_groups["render"].add(self)
+
+        self.time_count = 0         # Keeps track of game ticks since spawn
+        self.animation_rate = animation_rate    # Update the animation frame every x ticks
+        self.lifetime = lifetime         # Destroy this sprite when the time is up
+
+    def update(self):
+        self.time_count = self.time_count + 1
+
+        if self.time_count % self.animation_rate == self.animation_rate -1:
+            self.current_frame = self.current_frame + 1
+            if self.current_frame >= len(self.images):
+                self.current_frame = 0
+
+        self.image = self.images[self.current_frame]
+
+        if self.time_count >= self.lifetime:
+            self.destroy()
+
+    def destroy(self):
+        self.kill()
+        del(self)
--- lib/explosion.py
+++ lib/explosion.py
@@ -1,49 +1,25 @@
 import random
 import pygame
 
+from lib.animatedeffect import *
 from lib import resources
 
-class Explosion(pygame.sprite.Sprite):
+class Explosion(AnimatedEffect):
 
     def __init__(self, pos):
-        super().__init__()
         self.res = resources.Resources.instance()
         self.images = [
             self.res.assets['images']['explosion1'],
             self.res.assets['images']['explosion2'],
             self.res.assets['images']['explosion3'],
         ]
+        super().__init__(pos, self.images, 10, 80)
+        
         self.sounds = [
             self.res.assets['sounds']['explosions1'],
             self.res.assets['sounds']['explosions2'],
             self.res.assets['sounds']['explosions3'],
+            self.res.assets['sounds']['explosions4'],
         ]
-        self.current_frame = 0
-        self.image = self.images[self.current_frame]
-        self.rect = pygame.Rect(pos[0], pos[1], 1, 1)
-
-        self.res.update_groups["game"].add(self)
-        self.res.draw_groups["render"].add(self)
-
-        self.time_count = 0         # Keeps track of game ticks since spawn
-        self.animation_rate = 10    # Update the animation frame every x ticks
-        self.life_end = 60         # Destroy this sprite when the time is up
-
+        
         self.sounds[random.randint(0,len(self.sounds)-1)].play()
-
-    def update(self):
-        self.time_count = self.time_count + 1
-
-        if self.time_count % self.animation_rate == self.animation_rate -1:
-            self.current_frame = self.current_frame + 1
-            if self.current_frame >= len(self.sounds):
-                self.current_frame = 0
-
-        self.image = self.images[self.current_frame]
-
-        if self.time_count >= self.life_end:
-            self.destroy()
-
-    def destroy(self):
-        self.kill()
-        del(self)
