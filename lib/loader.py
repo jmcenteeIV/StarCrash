d6fc49661a286f5c5f8e5b2c307c3e3d7aa9853f
rrodrig6 <rrodrig6@uccs.edu>
2022-02-03 22:26:26 -0700

    Asset loading uses pyinstall TEMP path if available

--- lib/loader.py
+++ lib/loader.py
@@ -2,19 +2,16 @@ import os
 import sys
 import pygame
 
-import lib.utility as util
-
 # deprecate?
 def load_image(image_path: str):
     image = util.create_resource_path(image_path)
     return pygame.image.load(image).convert()
 
 def load_asset(asset_path: str, asset_type: str = 'image'):
-    resource_path = util.create_resource_path(asset_path)
 
     if asset_type == 'image' or asset_type == 'images':
-        return pygame.image.load(resource_path).convert_alpha()
+        return pygame.image.load(asset_path).convert_alpha()
     elif asset_type == 'sound' or asset_type == 'sounds':
-        return pygame.mixer.Sound(resource_path)
+        return pygame.mixer.Sound(asset_path)
     else:
         raise TypeError("Asset Type not supported!")
--- lib/utility.py
+++ lib/utility.py
@@ -1,11 +1,14 @@
 import os
 import sys
 
+
 def create_resource_path(resource_path):
     try:
     # PyInstaller creates a temp folder and stores path in _MEIPASS
+        print("MEIPASS: " + str(sys._MEIPASS))
         base_path = sys._MEIPASS
     except Exception:
+        print ("No MEIPASS")
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, resource_path)
